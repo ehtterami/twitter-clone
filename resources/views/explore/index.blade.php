31f4d88e3b35b149a8cc6f88847ed0dbdbc866bc
Ehtterami <ehtterami@gmail.com>
2025-09-10 14:27:54 +0330

    Enhance explore view

--- resources/views/explore/index.blade.php
+++ resources/views/explore/index.blade.php
@@ -3,23 +3,30 @@
 @section('title', 'Explore')
 
 @section('content')
-    <div class="container">
-        @forelse($posts as $post)
-            <div class="card">
-                <div class="card-body">
-                    {{ Str::limit($post->body, 50) }}
-                    <a href="#" class="btn btn-sm btn-outline-primary">View</a>
-                </div>
-                <div class="card-footer">
-                    {{ $post->created_at->diffForHumans() }}
-                </div>
-            </div>
-        @empty
-            <div class="card">
-                <div class="card-body">
-                    <p class="text-secondary">No Content Available Yet!</p>
-                </div>
-            </div>
-        @endforelse
-    </div>
+	<div class="container">
+		@forelse($posts as $post)
+			<div class="card mt-3">
+                <div class="card-header"><p class="fw-bold text-secondary fs-6 text-decoration-underline">{{ $post->user->name }}</p></div>
+				<div class="card-body">
+					{{ Str::limit($post->body, 50) }}
+					<div class="d-grid gap-0 mt-2">
+                        <a href="#" class="btn btn-sm btn-primary btn-block">View</a>
+                    </div>
+				</div>
+				<div class="card-footer">
+					{{ $post->created_at->diffForHumans() }}
+				</div>
+			</div>
+		@empty
+			<div class="card">
+				<div class="card-body">
+					<p class="text-secondary">No Content Available Yet!</p>
+				</div>
+			</div>
+		@endforelse
+
+		<div class="mt-3">
+            {{ $posts->links() }}
+        </div>
+	</div>
 @endsection
